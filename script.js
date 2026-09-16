bb5f758150e975f6b1ea8bbeebfe1361fa574e50
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-09-16 21:15:18 +1200

    adding todos

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ const itemsLeftContainerDesktop = document.getElementById(
   "items-left-container-desktop",
 );
 const items = document.getElementById("items");
-const item = document.querySelector(".item");
-const itemText = document.querySelector(".item-text");
 const filters = document.querySelector(".filters");
 const filtersDesktop = document.querySelector(".filters-desktop");
 const taskData = [];
 
+// Switch between light and dark.
 const enableDarkMode = () => {
   if (toggleMode.querySelector("img").src.includes("icon-moon.svg")) {
     toggleMode.querySelector("img").src = "./images/icon-sun.svg";
@@ -22,8 +21,6 @@ const enableDarkMode = () => {
     enterItem.classList.add("dark-mode");
     itemsLeftContainer.classList.add("dark-mode");
     itemsLeftContainerDesktop.classList.add("dark-mode");
-    item.classList.add("dark-mode");
-    itemText.classList.add("dark-mode");
     filters.classList.add("dark-mode");
     filtersDesktop.classList.add("dark-mode");
     body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
@@ -34,8 +31,6 @@ const enableDarkMode = () => {
     enterItem.classList.remove("dark-mode");
     itemsLeftContainer.classList.remove("dark-mode");
     itemsLeftContainerDesktop.classList.remove("dark-mode");
-    item.classList.remove("dark-mode");
-    itemText.classList.remove("dark-mode");
     filters.classList.remove("dark-mode");
     filtersDesktop.classList.remove("dark-mode");
     body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
@@ -46,15 +41,43 @@ toggleMode.addEventListener("click", () => {
   enableDarkMode();
 });
 
+// Making sure the item entered and added is in a clean and proper format.
 const validateInput = (input) => {
+  // Remove whitespace
   input = input.trim();
 
   if (!input) {
     return;
   }
 
+  // Capitalize first letter
   input = input.charAt(0).toUpperCase() + input.slice(1);
-  console.log(input);
+  console.log(`<div class="item">
+        <div class="item-content">
+          <div class="check-icon" tabindex="0">
+            <img src="./images/icon-check.svg" alt="check-item" />
+          </div>
+          <span class="item-text">${input}</span>
+        </div>
+        <img tabindex="0"
+          class="remove-icon"
+          src="./images/icon-cross.svg"
+          alt="remove-item"
+        />
+      </div>`);
+  items.innerHTML += `<div class="item">
+        <div class="item-content">
+          <div class="check-icon" tabindex="0">
+            <img src="./images/icon-check.svg" alt="check-item" />
+          </div>
+          <span class="item-text">${input}</span>
+        </div>
+        <img tabindex="0"
+          class="remove-icon"
+          src="./images/icon-cross.svg"
+          alt="remove-item"
+        />
+      </div>`;
 };
 
 addTodo.addEventListener("click", () => {
